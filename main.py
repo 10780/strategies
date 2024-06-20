aa86c9d5bfe16dae7815a012ad62da1f4bfad74c
garrett t <9407622+10780@users.noreply.github.com>
2024-06-19 22:53:12 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,9 @@ def main():
     random.seed()
     val = random.randint(0,9)
 
-    strategies = ["Look for any word or phrase","Work in silence",
-                  "Look at you hand and count down from five, say an idea",
-                  "Use physical tools","Must include X","Random image",
-                  "Describe an idea in one sentence, go from there",
-                  "Create with the fewest possible parts",
-                  "Collaborate, alternate contributing elements",
-                  "No editing"
-                  ]
+    f = open("strategies.txt", "r")
 
+    strategies = f.readlines()
     print("- Constraint challenge -\n")
     input("Press any key to continue\n")
     print("Your constraint: ")
